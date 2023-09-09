202b64625e5029fae9a88ab59d679f5f25e9d17e
mohammadNurullah <nurullahsarkar963@gmail.com>
2023-09-09 23:54:06 +0600

    modifyed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,41 @@
-const page = () => {
+"use client";
+// apps/register.js
+import { useState } from "react";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    // Send a POST request to your server to register the user
+    const response = await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.status === 200) {
+      // Registration successful, show a success message or redirect
+    } else {
+      // Handle registration error
+    }
+  };
+
   return (
     <div>
-      <h1>Home page</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleRegister}>Register</button>
     </div>
   );
-};
-
-export default page;
+}
